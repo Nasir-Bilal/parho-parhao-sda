9764a9f48abc2f689edadb77ce1d69cdf8b56c5c
babarr1 <i230040@isb.nu.edu.pk>
2025-11-22 07:45:48 -0800

    login and student side

--- src/main/java/com/example/demo/gLauncher.java
+++ src/main/java/com/example/demo/gLauncher.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class gLauncher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
